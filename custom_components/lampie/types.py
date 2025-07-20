f7b5dcc8e7e496ae211b760ee7c7ab003cd3c6e3
Whitney Young <wbyoung@users.noreply.github.com>
2025-07-20 16:38:05 -0700

    Add Z-Wave support for Red series switches
    This is currently untested.

--- custom_components/lampie/types.py
+++ custom_components/lampie/types.py
@@ -226,6 +226,7 @@ class Integration(StrEnum):
 
     ZHA = auto()
     Z2M = auto()
+    ZWAVE = auto()
 
 
 @dataclass(frozen=True)
@@ -269,6 +270,11 @@ class Z2MSwitchInfo:
     full_topic: str
     local_protection_enabled: bool | None = None
     double_tap_clear_notifications_disabled: bool | None = None
+
+
+@dataclass(frozen=True)
+class ZWaveSwitchInfo:
+    """ZWave switch info data class."""
 
 
 @dataclass(frozen=True)
